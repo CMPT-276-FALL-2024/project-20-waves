d3a6fa30033e94cb3469bfdb7ee68d4c26e14ddf
Karl <karlgo1217@gmail.com>
2024-11-09 14:11:54 -0800

    Created array for flash card info

---
+++ src/script.js
@@ -0,0 +1,14 @@
+//For Flashcards
+
+let flashcardInfo = [
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' },
+    { front : 'frontext', back : 'backtext' }
+];
