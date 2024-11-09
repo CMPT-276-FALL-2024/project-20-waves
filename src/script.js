09cc93a3f215f34f50fc51ffb91df0bf0c26a8d1
Karl <karlgo1217@gmail.com>
2024-11-09 14:44:15 -0800

    Added generate quiz button and added onhover change button colour event script to flash cards button

--- src/script.js
+++ src/script.js
@@ -12,3 +12,20 @@ let flashcardInfo = [
     { front : 'frontext', back : 'backtext' },
     { front : 'frontext', back : 'backtext' }
 ];
+
+//For options on practice page
+const flashcardsButton = document.querySelector(".flashcards-option");
+const flashCards = document.querySelector(".flashcard")
+
+flashcardsButton.addEventListener("mouseover", () => {
+   flashcardsButton.style.backgroundColor = 'darkgray';
+});
+
+flashcardsButton.addEventListener("mouseout", () => {
+    flashcardsButton.style.backgroundColor = 'lightgray';
+ });
+
+ flashcardsButton.addEventListener("click", () => {
+    flashCards.style.display = 'inline';
+ })
+
