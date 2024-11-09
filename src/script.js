8df5d7596b788f56dd7c300736a7f6fcb78c55f0
Karl <karlgo1217@gmail.com>
2024-11-09 15:04:07 -0800

    Added layout for quiz questions

--- src/script.js
+++ src/script.js
@@ -13,8 +13,11 @@ let flashcardInfo = [
     { front : 'frontext', back : 'backtext' }
 ];
 
+let flashCardNumber = 1;
+
 //For options on practice page
 const flashcardsButton = document.querySelector(".flashcards-option");
+const generateQuizButton = document.querySelector(".generate-quiz-button");
 const flashCards = document.querySelector(".flashcard")
 
 flashcardsButton.addEventListener("mouseover", () => {
@@ -25,7 +28,22 @@ flashcardsButton.addEventListener("mouseout", () => {
     flashcardsButton.style.backgroundColor = 'lightgray';
  });
 
+ generateQuizButton.addEventListener("mouseover", () => {
+    generateQuizButton.style.backgroundColor = 'darkgray';
+ });
+ 
+ generateQuizButton.addEventListener("mouseout", () => {
+    generateQuizButton.style.backgroundColor = 'lightgray';
+  });
+ 
+
  flashcardsButton.addEventListener("click", () => {
     flashCards.style.display = 'inline';
- })
+ });
+
+ generateQuizButton.addEventListener("click", () => {
+    flashCards.style.display = 'none';
+ });
+
+
 
