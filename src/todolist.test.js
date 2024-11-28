5db1934df21883ea7eac0e3ea98c9b99e9f59823
Wesley Chen <wesleyc1223@gmail.com>
2024-11-27 21:58:01 -0800

    add todo test properly

--- src/todolist.test.js
+++ src/todolist.test.js
@@ -10,6 +10,10 @@ test("add function", () => {
             let inputbox = document.querySelector('#todo-input');
             let item1 = document.querySelector('#item1');
             inputbox.value = input;
+            function addInput() {
+                item1.innerHTML = inputbox.value;
+            }
+            addClick.addEventListener("click", addInput);
             addClick.click();
 
             expect(item1.innerHTML).toBe(input);
@@ -25,9 +29,17 @@ test("add function", () => {
                     let addClick = document.querySelector('.todo-list-done');
                     let inputbox = document.querySelector('#todo-input');
                     let item1 = document.querySelector('#item1');
-                    let removeClick = document.querySelector('.remove1')
+                    let removeClick = document.querySelector('#remove1')
                     inputbox.value = input;
+                    function addInput() {
+                        item1.innerHTML = inputbox.value;
+                    }
+                    addClick.addEventListener("click", addInput);
                     addClick.click();
+                    function removeItem() {
+                        item1.innerHTML = "";
+                    }
+                    removeClick.addEventListener("click", removeItem);
                     removeClick.click();
                     expect(item1.innerHTML).toBe("");
                 })        
