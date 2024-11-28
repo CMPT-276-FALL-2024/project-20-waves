b7a72ca417820cb22d281a1be8e96749d87a8cbb
Wesley Chen <wesleyc1223@gmail.com>
2024-11-27 21:35:32 -0800

    added tests for todolist

---
+++ src/todolist.test.js
@@ -0,0 +1,33 @@
+test("add function", () => {
+    document.body.innerHTML = `
+            <button class = "todo-remove-button" id = "remove1">X</button><p id = "item1"></p>
+            <input class = "todo-input-box" id = "todo-input" placeholder="Enter Task Here">
+            <button class = "todo-list-done">
+                Done
+            </button>`
+            const input = "hii";
+            let addClick = document.querySelector('.todo-list-done');
+            let inputbox = document.querySelector('todo-input');
+            let item1 = document.querySelector(item1);
+            addClick.change(input, {target: {value: input}});
+            addClick.click();
+
+            expect(item1.innerHTML).toBe(input);
+        })
+ test("remove function", () => {
+            document.body.innerHTML = `
+                    <button class = "todo-remove-button" id = "remove1">X</button><p id = "item1"></p>
+                    <input class = "todo-input-box" id = "todo-input" placeholder="Enter Task Here">
+                    <button class = "todo-list-done">
+                        Done
+                    </button>`
+                    const input = "hii";
+                    let addClick = document.querySelector('.todo-list-done');
+                    let inputbox = document.querySelector('todo-input');
+                    let item1 = document.querySelector(item1);
+                    let removeClick = document.querySelector('.remove1')
+                    addClick.change(input, {target: {value: input}});
+                    addClick.click();
+                    removeClick.click();
+                    expect(item1.innerHTML).toBe("");
+                })        
