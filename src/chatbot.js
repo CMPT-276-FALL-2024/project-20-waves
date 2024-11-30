2033910db27fe4649646db75d1172163da414f32
Wesley Chen <wesleyc1223@gmail.com>
2024-11-30 01:51:35 -0800

    foirmateted code

--- src/chatbot.js
+++ src/chatbot.js
@@ -3,59 +3,57 @@ import { GoogleGenerativeAI } from "https://esm.sh/@google/generative-ai";
 const genAI = new GoogleGenerativeAI("AIzaSyD59cvRLGTGbqq-oQRidggkpYzrNrw1K_I");
 //dinfing variables
 const input = document.querySelector("#chatbot-input");
-const responsebox = document.getElementById('response');
-const enterbutton = document.querySelector('#chatbot-enter');
+const responsebox = document.getElementById("response");
+const enterbutton = document.querySelector("#chatbot-enter");
 let userInput;
 
-let userresponsearray = []
-
+let userresponsearray = [];
 
 function checkWhitespace(str) {
-    let whitespace = new Set([" ", "\t", "\n", ""]);
-        let j = 0;
-        for (let i =0; i<str.length; i++){
-            if (str[i] == " "){
-                j++;
-            }
-        }
-        if (str == ""||j == str.length ) {
-            return true;
-        }
-    return false;
+  let whitespace = new Set([" ", "\t", "\n", ""]);
+  let j = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == " ") {
+      j++;
+    }
+  }
+  if (str == "" || j == str.length) {
+    return true;
+  }
+  return false;
 }
 
-input.addEventListener("keyup", ({key}) => {
-    if(key === "Enter"){
-        if (!checkWhitespace(input.value) ){
-            console.log(input.value);
-            userInput = input.value
-            geminiAPI();
-            
-            input.value = "";
-        }}
+input.addEventListener("keyup", ({ key }) => {
+  if (key === "Enter") {
+    if (!checkWhitespace(input.value)) {
+      console.log(input.value);
+      userInput = input.value;
+      geminiAPI();
 
- })
+      input.value = "";
+    }
+  }
+});
 
-enterbutton.addEventListener("click", () => { 
-        if (!checkWhitespace(input.value) ){
-            console.log(input.value);
-            userInput = input.value
-            geminiAPI();
-            
-            input.value = "";
-        }
- })
+enterbutton.addEventListener("click", () => {
+  if (!checkWhitespace(input.value)) {
+    console.log(input.value);
+    userInput = input.value;
+    geminiAPI();
+
+    input.value = "";
+  }
+});
 
 //gemini prompt fucntion
 async function geminiAPI() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const prompt = userInput;
 
-    const prompt = userInput;
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-
-    const text = response.text();
-    responsebox.innerHTML = text;
+  const text = response.text();
+  responsebox.innerHTML = text;
 }
