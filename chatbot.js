04e49c39c87874f22e561ef6168a6bc1ed52b4f8
Wesley Chen <wesleyc1223@gmail.com>
2024-12-05 17:52:10 -0800

    documented chatbot.js and removed redundant code

--- chatbot.js
+++ chatbot.js
@@ -1,66 +1,59 @@
+//set up Gemini API
 import { GoogleGenerativeAI } from "https://esm.sh/@google/generative-ai";
-
 const genAI = new GoogleGenerativeAI("AIzaSyD59cvRLGTGbqq-oQRidggkpYzrNrw1K_I");
-//dinfing variables
+//dinfing html elements
 const input = document.querySelector("#chatbot-input");
 const responsebox = document.getElementById("response");
 const enterbutton = document.querySelector(".chatbot-enter");
+//defin variables for user input
 let userInput;
 
-let userresponsearray = [];
-
-function checkWhitespace(str) {
-  let whitespace = new Set([" ", "\t", "\n", ""]);
+function checkWhitespace(str) {//funciton to check if str is a whitespace
   let j = 0;
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {//iterates through all values of string
     if (str[i] == " ") {
-      j++;
+      j++;//if string has whitespace for character, i , then keep trakc of it
     }
   }
-  if (str == "" || j == str.length) {
+  if (str == "" || j == str.length) {//if the amopunt of white space is = amount of characters, then the string is all white space
     return true;
   }
   return false;
 }
 
+function sendInput(string){ //function to send user input
+  if (!checkWhitespace(input.value)) { //check for validity
+    userInput = input.value; //assign input to userinput vairable to be used in gemeiniAPI fucniton
+    geminiAPI(); //run function to send text to api
+    input.value = ""; //reset input box
+  }
+  else{//error case
+    responsebox.innerHTML = "ERROR: please enter a message"; //displya errror to user
+    responsebox.style.color = "#ff0033" //change to text red
+  }
+}
+//adds event listeners for input
 input.addEventListener("keyup", ({ key }) => {
   if (key === "Enter") {
-    if (!checkWhitespace(input.value)) {
-      userInput = input.value;
-      geminiAPI();
-      
-      input.value = "";
-    }
-    else{
-      responsebox.innerHTML = "ERROR: Please enter a message";
-      responsebox.style.color = "#ff0033"
-    }
+    sendInput(input.value);
   }
 });
 
 enterbutton.addEventListener("click", () => {
-  if (!checkWhitespace(input.value)) {
-    userInput = input.value;
-    geminiAPI();
-
-    input.value = "";
-  }
-  else{
-    responsebox.innerHTML = "ERROR: please enter a message";
-    responsebox.style.color = "#ff0033"
-  }
+  sendInput(input.value);
 });
 
 //gemini prompt fucntion
 async function geminiAPI() {
+  //define model used
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
+  //set up prompt to send
   const prompt = userInput;
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-
-  const text = response.text();
+  //retrieve resuilts
+  const result = await model.generateContent(prompt); //gets resuklt
+  const response = await result.response; //gets reposnse
+  const text = response.text(); //get text
+  //display reponse form api
   responsebox.innerHTML = text;
-  responsebox.style.color = "#000000"
+  responsebox.style.color = "#000000" //changes text colour back to black
 }
